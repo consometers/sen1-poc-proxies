2737d444375e75dcde05f4a5a44b2111121c8fe0
root <root@srv3.consometers.org>
2019-03-25 21:08:17 +0100

    debug script install

--- scripts/linux/install-proxy.sh
+++ scripts/linux/install-proxy.sh
@@ -11,23 +11,23 @@
 
 # Config du user
 
-echo -n "Java environnement"
-echo -n "------------------"
+echo "Java environnement"
+echo "------------------"
 read -p "Java home: " JAVA_HOME
 read -p "Grails home: " GRAILS_HOME
 read -p "Catalina home: " CATALINA_HOME
 
-echo -n "Build environnement"
-echo -n "-------------------"
+echo "Build environnement"
+echo "-------------------"
 read -p "Proxy path: " PROXY_PATH
 
-echo -n "Deploy environnement"
-echo -n "--------------------"
+echo "Deploy environnement"
+echo "--------------------"
 read -p "Instance path: " INSTANCE_PATH
-read -p "HTTP port: " HTTP_PORT
-read -p "HTTPS port: " HTTPS_PORT
-read -p "AJP port: " AJP_PORT
-read -p "SHUTDOWN port: " SHUTDOWN_PORT
+read -p "HTTP port [default=8080]: " HTTP_PORT
+read -p "HTTPS port [default=8443]: " HTTPS_PORT
+read -p "AJP port [default=8009]: " AJP_PORT
+read -p "SHUTDOWN port [default=8005]: " SHUTDOWN_PORT
 
 
 # Init variable
@@ -42,11 +42,11 @@ PATH_SCRIPT="/usr/local/bin"
 
 cat <<EOF > ${PATH_SCRIPT}/${PROXY_NAME}-build.sh
 #!/bin/sh
-JAVA_HOME="$JAVA_HOME"
-GRAILS_HOME="$GRAILS_HOME"
+export JAVA_HOME="$JAVA_HOME"
+export GRAILS_HOME="$GRAILS_HOME"
 cd $PROXY_PATH
 git pull origin master
-$GRAILS_HOME/bin/grails war
+\${GRAILS_HOME}/bin/grails war
 EOF
 
 chmod +x ${PATH_SCRIPT}/${PROXY_NAME}-build.sh
@@ -60,34 +60,39 @@ cat <<EOF > ${PATH_SCRIPT}/${PROXY_NAME}-deploy.sh
 #!/bin/sh
 read -p "Instance ID [0-9]: " INSTANCE_ID
 read -p "Proxy version [x.y.z]: " PROXY_VERSION
-INSTANCE_NAME="${PROXY_NAME}-${INSTANCE_ID}"
-INSTANCE="$INSTANCE_PATH/$INSTANCE_NAME"
-WAR_FILE="$PROXY_PATH/build/libs/${PROXY_NAME}-${PROXY_VERSION}"
+INSTANCE_NAME="${PROXY_NAME}-\${INSTANCE_ID}"
+INSTANCE="$INSTANCE_PATH/\$INSTANCE_NAME"
+WAR_FILE="$PROXY_PATH/build/libs/${PROXY_NAME}-\${PROXY_VERSION}.war.original"
 
-if [ ! -f "$WAR_FILE" ]; then
-  echo "War not exist ! ($WAR_FILE)"
-  exit 1
-fi
-
-if [ -z "$INSTANCE_ID" ]; then
+if [ -z "\$INSTANCE_ID" ]; then
   echo "Instance ID is required !"
   exit 1
 fi
 
-rm -r $INSTANCE
-cp -r $CATALINA_HOME $INSTANCE
-mkdir $INSTANCE/logs
-mkdir $INSTANCE/temp
-mkdir $INSTANCE/work
-mkdir $INSTANCE/webapps
-cp $WAR_FILE $INSTANCE/webapps/${PROXY_NAME}.war
+if [ -z "\$PROXY_VERSION" ]; then
+  echo "Proxy version is required !"
+  exit 1
+fi
 
-sed -i -e "s/serverId.pid/${INSTANCE_NAME}.pid/g" $INSTANCE/bin/setenv.sh
-sed -i -e "s/serverId=serverId/serverId=${INSTANCE_NAME}/g" $INSTANCE/bin/setenv.sh
-sed -i -e "s/port=\"8005\"/port=\"${INSTANCE_ID}${SHUTDOWN_PORT}\"/g" $INSTANCE/conf/server.xml
-sed -i -e "s/port=\"8080\"/port=\"${INSTANCE_ID}${HTTP_PORT}\"/g" $INSTANCE/conf/server.xml
-sed -i -e "s/redirectPort=\"8443\"/redirectPort=\"${INSTANCE_ID}${HTTPS_PORT}\"/g" $INSTANCE/conf/server.xml
-sed -i -e "s/port=\"8009\"/port=\"${INSTANCE_ID}${AJP_PORT}\"/g" $INSTANCE/conf/server.xml
+if [ ! -f "\$WAR_FILE" ]; then
+  echo "War not exist ! (\$WAR_FILE)"
+  exit 1
+fi
+
+rm -r \$INSTANCE
+cp -r $CATALINA_HOME \$INSTANCE
+mkdir \$INSTANCE/logs
+mkdir \$INSTANCE/temp
+mkdir \$INSTANCE/work
+mkdir \$INSTANCE/webapps
+cp \$WAR_FILE \$INSTANCE/webapps/${PROXY_NAME}.war
+
+sed -i -e "s/serverId.pid/\${INSTANCE_NAME}.pid/g" \$INSTANCE/bin/setenv.sh
+sed -i -e "s/serverId=serverId/serverId=\${INSTANCE_NAME}/g" \$INSTANCE/bin/setenv.sh
+sed -i -e "s/port=\"8005\"/port=\"${INSTANCE_ID}${SHUTDOWN_PORT:-8005}\"/g" \$INSTANCE/conf/server.xml
+sed -i -e "s/port=\"8080\"/port=\"${INSTANCE_ID}${HTTP_PORT:-8080}\"/g" \$INSTANCE/conf/server.xml
+sed -i -e "s/redirectPort=\"8443\"/redirectPort=\"${INSTANCE_ID}${HTTPS_PORT:-8443}\"/g" \$INSTANCE/conf/server.xml
+sed -i -e "s/port=\"8009\"/port=\"${INSTANCE_ID}${AJP_PORT:-8009}\"/g" \$INSTANCE/conf/server.xml
 EOF
 
 chmod +x ${PATH_SCRIPT}/${PROXY_NAME}-deploy.sh
@@ -99,25 +104,25 @@ chmod +x ${PATH_SCRIPT}/${PROXY_NAME}-deploy.sh
 cat <<EOF > ${PATH_SCRIPT}/${PROXY_NAME}-start.sh
 #!/bin/sh
 read -p "Instance ID [0-9]: " INSTANCE_ID
-INSTANCE_NAME="${PROXY_NAME}-${INSTANCE_ID}"
-INSTANCE="$INSTANCE_PATH/$INSTANCE_NAME"
+INSTANCE_NAME="${PROXY_NAME}-\${INSTANCE_ID}"
+INSTANCE="$INSTANCE_PATH/\$INSTANCE_NAME"
 export JAVA_HOME="$JAVA_HOME"
 export CATALINA_HOME="$CATALINA_HOME"
-export CATALINA_BASE="$INSTANCE"
-cd $CATALINA_BASE
-$CATALINA_HOME/bin/startup.sh
+export CATALINA_BASE="\$INSTANCE"
+cd \$CATALINA_BASE
+\$CATALINA_HOME/bin/startup.sh
 EOF
 
 
-cat << EOF > $INSTANCE_DIR/$SERVER_ID-stop.sh
+cat << EOF > ${PATH_SCRIPT}/${PROXY_NAME}-stop.sh
 #!/bin/sh
 read -p "Instance ID [0-9]: " INSTANCE_ID
-INSTANCE_NAME="${PROXY_NAME}-${INSTANCE_ID}"
-INSTANCE="$INSTANCE_PATH/$INSTANCE_NAME"
+INSTANCE_NAME="${PROXY_NAME}-\${INSTANCE_ID}"
+INSTANCE="$INSTANCE_PATH/\$INSTANCE_NAME"
 export JAVA_HOME="$JAVA_HOME"
 export CATALINA_HOME="$CATALINA_HOME"
-export CATALINA_BASE="$INSTANCE"
-$CATALINA_HOME/bin/shutdown.sh
+export CATALINA_BASE="\$INSTANCE"
+\$CATALINA_HOME/bin/shutdown.sh
 EOF
 
 chmod +x ${PATH_SCRIPT}/${PROXY_NAME}-start.sh
